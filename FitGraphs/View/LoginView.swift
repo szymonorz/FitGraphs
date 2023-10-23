f74f68b0833380b6105d1f679477d6992d1b165a
szyorz <originalemail001@gmail.com>
2023-10-24 01:02:36 +0200

    UI: Logo + Login Screen (WIP)

--- FitGraphs/View/LoginView.swift
+++ FitGraphs/View/LoginView.swift
@@ -18,11 +18,22 @@ struct LoginView: View {
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack {
+            VStack(spacing: 0) {
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 128, height: 128)
+                Text("FitGraphs")
+                    .frame(width: 256, height: 128)
+                    .font(.system(size: 60))
+                    .fontWeight(.bold)
+                    .fontWidth(.condensed)
+                    .foregroundStyle(.black)
                 GoogleSignInButton {
                     viewStore.send(GoogleAuthReducer.Action.signIn)
-                }
-            }
+                }.buttonBorderShape(.capsule)
+                    .frame(width: 256)
+                    .padding(.top, 50)
+            }.frame(alignment: .top)
         }
     }
 }
